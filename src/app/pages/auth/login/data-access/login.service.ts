62181f86bae2c2279f1ae4beccb7f48f82b30696
Joshua Morony <joshua.morony@gmail.com>
2024-01-18 16:15:38 +1030

    migrate

--- src/app/pages/auth/login/data-access/login.service.ts
+++ src/app/pages/auth/login/data-access/login.service.ts
@@ -1,38 +1,41 @@
 import { signal } from '@angular/core';
 import { connect } from 'ngxtension/connect';
 import { createService } from 'ngxtension/create-injection-token';
-import { Subject, merge, switchMap } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Subject, merge, switchMap, from, EMPTY } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { injectAuthService } from '../../../../shared/data-access/auth.service';
 import type { Credentials } from '../../../../shared/interfaces/credentials';
 
 export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
 
 export const [injectLoginService, provideLoginService] = createService(
-	() => {
-		const authService = injectAuthService();
+  () => {
+    const authService = injectAuthService();
 
-		const error$ = new Subject<any>();
-		const login$ = new Subject<Credentials>();
+    const error$ = new Subject<any>();
+    const login$ = new Subject<Credentials>();
 
-		const userAuthenticated$ = login$.pipe(
-			switchMap((credentials) =>
-				authService.login(credentials).catch((err) => {
-					error$.next(err);
-					return null as never;
-				}),
-			),
-		);
-		const nextStatus$ = merge(
-			userAuthenticated$.pipe(map(() => 'success' as const)),
-			login$.pipe(map(() => 'authenticating' as const)),
-			error$.pipe(map(() => 'error' as const)),
-		);
+    const userAuthenticated$ = login$.pipe(
+      switchMap((credentials) =>
+        from(authService.login(credentials)).pipe(
+          catchError((err) => {
+            error$.next(err);
+            return EMPTY
+          })
+        )
+      ),
+    );
 
-		const status = signal<LoginStatus>('pending');
-		connect(status, nextStatus$);
+    const nextStatus$ = merge(
+      userAuthenticated$.pipe(map(() => 'success' as const)),
+      login$.pipe(map(() => 'authenticating' as const)),
+      error$.pipe(map(() => 'error' as const)),
+    );
 
-		return { status: status.asReadonly(), login$ };
-	},
-	{ isRoot: false },
+    const status = signal<LoginStatus>('pending');
+    connect(status, nextStatus$);
+
+    return { status: status.asReadonly(), login$ };
+  },
+  { isRoot: false },
 );
